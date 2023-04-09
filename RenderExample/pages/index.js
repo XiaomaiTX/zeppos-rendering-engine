5196579d8f2b48eaa787050585803f8d9d02dc16
xiaomaitx <381003647@qq.com>
2023-04-09 15:25:49 +0800

    New 3D model preset: Polyline

--- RenderExample/pages/index.js
+++ RenderExample/pages/index.js
@@ -13,17 +13,17 @@ import { Time } from "@zos/sensor";
 const logger = log.getLogger("index.page");
 
 Page({
-	onCreate(e) {
-		logger.log("app on create invoke");
-	},
-	onInit() {
-		logger.log("page on init invoke");
-	},
-	onShow() {
-		logger.log("page on show invoke");
-	},
-	build() {
-		/*
+  onCreate(e) {
+    logger.log("app on create invoke");
+  },
+  onInit() {
+    logger.log("page on init invoke");
+  },
+  onShow() {
+    logger.log("page on show invoke");
+  },
+  build() {
+    /*
 		const strokeRect = createWidget(widget.STROKE_RECT, {
 			x: 0,
 			y: 0,
@@ -33,27 +33,34 @@ Page({
 			line_width: 2,
 			color: 0xffffff,
 		});//*/
-		console.log("Device w&h : " + DEVICE_WIDTH + "x" + DEVICE_HEIGHT);
-		const scene = new ZeppRE();
-		scene.createScene(); // 创建场景 scene
-		scene.createCamera({
-			position: [0, 0, 0],
-			fov: 60,
-			aspectRatio: 1,
-			nearPlane: 1,
-			farPlane: 10,
-		});
-		const cube = new ZeppRE();
-		cube.createModel({
-			model: "CUBE",
-		}); // 创建一个立方体模型 cube
-		scene.add(cube);
-		// DEBUG 列出场景中的所有模型
-		scene.models.forEach((model) => {
-			console.log("Models in scene : " + model.name);
-		});
-		scene.render();
-
+    console.log("Device w&h : " + DEVICE_WIDTH + "x" + DEVICE_HEIGHT);
+    const scene = new ZeppRE();
+    scene.createScene(); // 创建场景 scene
+    scene.createCamera({
+      position: [0, 0, 0],
+      fov: 60,
+      aspectRatio: 1,
+      nearPlane: 1,
+      farPlane: 10,
+    });
+    const cube = new ZeppRE();
+    cube.createModel({
+      model: "POLYLINE",
+      geometry: {
+        points: [
+          [0, 0, 0],
+          [10, 10, 10],
+          [20, 30, 40],
+          [-20, 30, 40],
+          [12, -60, 52],
+          [42, 43, -40],
+        ],
+      },
+    }); 
+    scene.add(cube);
+    // DEBUG 列出场景中的所有模型
+    scene.render();
+    /*
 		const timer = new ZeppTimer(() => {
 			console.log(
 				"scene.models[0].geometry.direction[1] " +
@@ -67,13 +74,13 @@ Page({
 			);
 
 			scene.render();
-		}, 1000 / 60);
-		timer.start();
-	},
-	onHide() {
-		logger.log("page on hide invoke");
-	},
-	onDestroy() {
-		logger.log("page on destroy invoke");
-	},
+		}, 1000 / 45);
+		timer.start();//*/
+  },
+  onHide() {
+    logger.log("page on hide invoke");
+  },
+  onDestroy() {
+    logger.log("page on destroy invoke");
+  },
 });
